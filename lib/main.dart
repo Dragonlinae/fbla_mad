6382c2a948c72f7635b1a6393c0c8da46e5bfe38
Ericspk33 <38512469+Ericspk33@users.noreply.github.com>
2020-01-12 15:37:06 -0800

    testing HELLO

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo adding Eric?',
+      title: 'Flutter Demo adding Er',
       theme: ThemeData(
         // This is the theme of your application.
         //

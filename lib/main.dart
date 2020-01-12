28048e0148556be5ded135f0b0e99bda4b5f254d
Joshua Hsu <wolfcorprobotics12525@gmail.com>
2020-01-12 15:40:24 -0800

    double push

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo adding Er',
+      title: 'Flutter Demo adding Erik',
       theme: ThemeData(
         // This is the theme of your application.
         //
